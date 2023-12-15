049116c66e323b50bdb0386f19285f554b4f4f05
alikhabazian <khabaziana@gmail.com>
2023-12-15 14:07:39 +0330

    add acceptance and reject and photo and doc

--- functions.py
+++ functions.py
@@ -1,8 +1,13 @@
 from constant import *
 from telegram import Update,ReplyKeyboardMarkup,InlineKeyboardButton,InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler, CallbackContext
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+RECEIVER=os.getenv("RECEIVER")
 PREORDER_STATE, PREORDER_CHOOSE, PREORDER_CONFIRMED_STATE, PREORDER_CANCEL_STATE = range(4)
+
 async def start(update: Update, context: CallbackContext) -> int:
     await update.message.reply_text(
         text=WELCOME_MESSAGE,
@@ -22,6 +27,9 @@ async def preorderChoose(update: Update, context: CallbackContext) -> int:
     return PREORDER_CHOOSE
 
 async def waitingRecipt(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text=f"{update.callback_query.data} has been chosen")
     keyboard = [
         [InlineKeyboardButton('🚫 Cancel', callback_data='Cancel'),]
     ]
@@ -52,8 +60,7 @@ async def userTextRecipt(update: Update, context: CallbackContext) -> int:
     context.user_data['user_id'] = update.effective_chat.id
     context.user_data['payment_receipt'] = update.message.text
     await context.bot.send_message(
-        # TODO
-        chat_id=update.effective_chat.id,
+        chat_id=RECEIVER,
         text=
         f"Pre-order\n" +
         f"payment_method:{context.user_data['payment_method']}\n" +
@@ -65,8 +72,7 @@ async def userTextRecipt(update: Update, context: CallbackContext) -> int:
         reply_markup=reply_markup
     )
     return ConversationHandler.END
-async def preorderImage(update: Update, context: CallbackContext) -> int:
-    print(3)
+async def userImageRecipt(update: Update, context: CallbackContext) -> int:
     keyboard = [
         [InlineKeyboardButton('❌ Reject', callback_data='Reject'),
          InlineKeyboardButton('✅ Accept', callback_data='Accept')]
@@ -81,8 +87,31 @@ async def preorderImage(update: Update, context: CallbackContext) -> int:
 
     await context.bot.send_photo(
         # TODO
-        chat_id=update.effective_chat.id,
+        chat_id=RECEIVER,
         photo=update.message.photo[0].file_id,
+        caption=
+        f"Pre-order\n" +
+        f"payment_method:{context.user_data['payment_method']}\n" +
+        (f"username:@{context.user_data['username']}\n" if (context.user_data['username'] is not None) else "") +
+        f"user_id:{context.user_data['user_id']}\nfull_name:{context.user_data['full_name']}\n",
+        reply_markup=reply_markup
+    )
+    return ConversationHandler.END
+
+async def userDocRecipt(update: Update, context: CallbackContext) -> int:
+    keyboard = [
+        [InlineKeyboardButton('❌ Reject', callback_data='Reject'),
+         InlineKeyboardButton('✅ Accept', callback_data='Accept')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+
+    context.user_data['full_name'] = update.effective_chat.full_name
+    context.user_data['username'] = update.effective_chat.username
+    context.user_data['user_id'] = update.effective_chat.id
+    await context.bot.send_document(
+        chat_id=RECEIVER,
+        document=update.message.document.file_id,
         caption=
         f"Pre-order\n" +
         f"payment_method:{context.user_data['payment_method']}\n" +
@@ -94,7 +123,64 @@ async def preorderImage(update: Update, context: CallbackContext) -> int:
 
 
 
-
-
 async def preorder_cancel(update: Update, context: CallbackContext) -> int:
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text(text="Canceled")
     return ConversationHandler.END
+
+
+async def acceptHandeling(update: Update, context: CallbackContext) -> int:
+    credentials = get_user_credentials(update.effective_message)
+    await context.bot.send_message(
+        chat_id=credentials['user_id'],
+        text=ACCEPTED
+    )
+    if update.effective_message.text:
+        await update.effective_message.edit_text(
+            update.effective_message.text+
+            "\n-------------------------"+
+            "\nAdmin Accept : "+
+            f"{update.callback_query.from_user.full_name}"
+            )
+    elif update.effective_message.caption:
+        await update.effective_message.edit_caption(
+            update.effective_message.caption+
+            "\n-------------------------"+
+            "\nAdmin Accept : "+
+            f"{update.callback_query.from_user.full_name}"
+            )
+
+async def rejectHandeling(update: Update, context: CallbackContext) -> int:
+    credentials = get_user_credentials(update.effective_message)
+    await context.bot.send_message(
+        chat_id=credentials['user_id'],
+        text=REJECTED
+    )
+
+
+
+def get_user_credentials(effective_message):
+    lines = []
+    payment_receipt = None
+    credentials = {}
+    if effective_message.text:
+        payment_receipt = effective_message.text.split('-------------------------')[1]
+        lines = effective_message.text.splitlines()  # Split the string into lines
+
+    elif effective_message.caption:
+        print(f"update.effective_message:{type(effective_message)}")
+
+        if effective_message.document:
+            payment_receipt = effective_message.document.file_id
+        elif effective_message.photo:
+            payment_receipt = effective_message.photo[0].file_id
+        lines = effective_message.caption.splitlines()  # Split the string into lines
+    credentials['payment_receipt'] = payment_receipt
+    for line in lines:
+        if 'user_id' in line:
+            credentials['user_id'] = int(line.split(':')[1])
+        elif 'payment_method' in line:
+            credentials['payment_method'] = line.split(':')[1]
+    return credentials
+
--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 from constant import *
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ConversationHandler, CallbackContext,CallbackQueryHandler
-from dotenv import load_dotenv
 from functions import *
 
+# TODO cleaning code
 load_dotenv()
-BOT_TOKEN=bot_token = os.getenv("BOT_TOKEN")
+BOT_TOKEN= os.getenv("BOT_TOKEN")
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     mainCommandHandler=CommandHandler("start", start)
-    preorederConversationHandler=ConversationHandler(
+    preorderConversationHandler=ConversationHandler(
         entry_points=[CommandHandler("preorder", preorderChoose)],
         states={
             PREORDER_CHOOSE:[
                 CallbackQueryHandler(waitingRecipt,pattern='^Rial$'),
-                CallbackQueryHandler(waitingRecipt,pattern='^Paypal'),
+                CallbackQueryHandler(waitingRecipt,pattern='^Paypal$'),
                 # MessageHandler(filters=filters.Regex(r'^[\s\S]*$'), user_charge_acc_inputed),
             ],
             PREORDER_STATE: [
                 MessageHandler(filters=filters.Regex(r'^[\s\S]*$'), callback=userTextRecipt),
-                # MessageHandler(filters=filters.PHOTO, callback=userImageRecipt),
-                # MessageHandler(filters=filters.Document.ALL, userDocRecipt),
+                MessageHandler(filters=filters.PHOTO, callback=userImageRecipt),
+                MessageHandler(filters=filters.Document.ALL, callback=userDocRecipt),
             ],
         },
         fallbacks=[CommandHandler("cancel", preorder_cancel)],
     )
+    acceptQueryHandler = CallbackQueryHandler(acceptHandeling,pattern='^Accept$')
+    rejectQueryHandler = CallbackQueryHandler(acceptHandeling,pattern='^Reject$')
+
+
     application.add_handler(mainCommandHandler)
-    application.add_handler(preorederConversationHandler)
+    application.add_handler(preorderConversationHandler)
+    application.add_handler(acceptQueryHandler)
+    application.add_handler(rejectQueryHandler)
     application.run_polling()
